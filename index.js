6f296bd8d99e0168e2e302b25445d8d9b29c57da
jonayed-nasir <junayednasir073@gmail.com>
2025-07-27 12:50:05 +0600

    JavaScript To-Do app js button click event done

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const addBtn = document.querySelector('#addBtn');
 const inputBox = document.querySelector('#inputBox');
 const outputAll = document.querySelector('#outputAll');
 
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const text = inputBox.value.trim();
+  if (text === '') {
+    alert('inputBox');
+    return;
+  }
+
+//   createTodo(text);
+  inputBox.value = '';
+});
+
